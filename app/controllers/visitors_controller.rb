4304f07cec657d0418a10678e2264a6ef462bd15
tomazini <tome.ribeiro@gmail.com>
2014-08-24 16:47:05 +0100

    simple form

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,10 +1,17 @@
 class VisitorsController < ApplicationController
-def new
-Rails.logger.debug "\033[1;34;40m[DEBUG]\033[0m " + "entering new method"
-@owner = Owner.new
-Rails.logger.debug "\033[1;34;40m[DEBUG]\033[0m " + "Owner name is"  + @owner.name
+
+#def new
+#Rails.logger.debug "\033[1;34;40m[DEBUG]\033[0m " + "entering new method"
+#@owner = Owner.new
+#Rails.logger.debug "\033[1;34;40m[DEBUG]\033[0m " + "Owner name is"  + @owner.name
 
 #raise 'Deliberate Failure'
+#end
 
+def new
+@owner = Owner.new
+flash.now[:notice] = 'Welcome!'
+flash.now[:alert] = 'My birthday is soon.'
 end
+
 end
