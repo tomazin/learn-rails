c5f2c387d25871c3010a705338c6ce02cd43cd70
tomazin <tome.ribeiro@gmail.com>
2014-09-08 21:57:17 +0100

    update application layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -8,10 +8,10 @@ class VisitorsController < ApplicationController
 #raise 'Deliberate Failure'
 #end
 
-def new
+def new	
 @owner = Owner.new
-flash.now[:notice] = 'Welcome!'
-flash.now[:alert] = 'My birthday is soon.'
+flash[:notice] = 'Welcome!'
+flash[:alert] = 'My birthday is soon.'
 end
 
 end
